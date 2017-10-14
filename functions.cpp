9a77d4e78c64aa9c9d2d63066ca076db0d876f18
Egahi <okworisamuel6@gmail.com>
2017-10-15 00:28:18 +0100

    indent all blocks of code

--- functions.cpp
+++ functions.cpp
@@ -21,23 +21,23 @@ char dWord[LIMIT][WIDTH];
  */
 void welcome (void)
 {
-    cout << "\nHello! thanks for using num2word.\n"
-				   << "This program converts a number to words!\n"
-				   << "Input a number to get started." << endl;
-		
-    input = 0;
-		 length = 0;
-			
-			// initialise arrays with null indicator to clear any previous input	
-    for (int i = 0; i < LIMIT; i++)
-			{
-		     digits[i] = {'\0'};
-								
-        for (int j = 0; j < WIDTH; j++)
-        {
-             dWord[i][j] = {'\0'};
-        }
-			}
+	cout << "\nHello! thanks for using num2word.\n"
+	 << "This program converts a number to words!\n"
+	 << "Input a number to get started." << endl;
+
+	input = 0;
+	length = 0;
+
+	// initialise arrays with null indicator to clear any previous input
+	for (int i = 0; i < LIMIT; i++)
+	{
+		digits[i] = {'\0'};
+
+		for (int j = 0; j < WIDTH; j++)
+		{
+		     dWord[i][j] = {'\0'};
+		}
+	}
 }
 
 /**
@@ -46,36 +46,36 @@ void welcome (void)
  */
 void getInput(void)
 {
-    // variable to validate input
-    bool validate;
-						
-    // accept only numbers between 0 and 999 999 999 999
-    do
-    {
-        cout << "\nNumber: ";
-							
-					 // constrain input to numbers	
-        if (!(cin >> input))
-        {
-            cout << "Enter only numbers between " << MIN
-												<< " and " << MAX << " inclusive." << endl;
-            cin.clear();
-            cin.ignore(15, '\n');
-								 validate = false;
-        }
-						
-					 // constrain number to range 0 - 999 999 999 999
-        else if (input < MIN || input > MAX)
-        {
-            cout << "Enter a number between " << MIN
-												<< " and " << MAX << " inclusive." << endl;
-            cin.ignore(15, '\n');
-							  validate = false;
-        }
-								
-        else
-            validate = true;
-    } while (validate == false);
+	// variable to validate input
+	bool validate;
+
+	// accept only numbers between 0 and 999 999 999 999
+	do
+	{
+		cout << "\nNumber: ";
+
+		// constrain input to numbers
+		if (!(cin >> input))
+		{
+			cout << "Enter only numbers between " << MIN
+			     << " and " << MAX << " inclusive." << endl;
+			cin.clear();
+			cin.ignore(15, '\n');
+			validate = false;
+		}
+
+		// constrain number to range 0 - 999 999 999 999
+		else if (input < MIN || input > MAX)
+		{
+			cout << "Enter a number between " << MIN
+			     << " and " << MAX << " inclusive." << endl;
+			cin.ignore(15, '\n');
+			validate = false;
+		}
+
+		else
+			validate = true;
+	} while (validate == false);
 }
 
 /**
@@ -88,36 +88,36 @@ void getInput(void)
  */ 
 void splitNum(void)
 {
-    for (int i = MIN; i < LIMIT; i++)
-    {
-        // number is 0 exit loop!
-        if (input == MIN)
-        {
-									// number is 0 which is a distinct number
-								 if (length == MIN)
-                length = 1;
+	for (int i = MIN; i < LIMIT; i++)
+	{
+		// number is 0 exit loop!
+		if (input == MIN)
+		{
+			// number is 0 which is a distinct number
+			if (length == MIN)
+				length = 1;
 
-            // number is zero or has been successfilly split up exit loop
-            digits[i] = input;
-            break;
-        }
+			// number is zero or has been successfilly split up exit loop
+			digits[i] = input;
+			break;
+		}
 
-        // pick last 2 digits out of those left, as a single number
-        if (i % 2 == MIN)
-        {
-            digits[i] = input % 100;
-            input /= 100;
-        }
+		// pick last 2 digits out of those left, as a single number
+		if (i % 2 == MIN)
+		{
+			digits[i] = input % 100;
+			input /= 100;
+		}
 
-        // pick last digit out of those left, as a single number
-        else
-        {
-            digits[i] = input % 10;
-            input /= 10;
-        }
+		// pick last digit out of those left, as a single number
+		else
+		{
+			digits[i] = input % 10;
+			input /= 10;
+		}
 
-        length += 1;
-    }
+		length += 1;
+	}
 }
 
 /**
@@ -125,54 +125,54 @@ void splitNum(void)
  */
 bool numToWord(void)
 {
-    int check = MIN;
-    
-    for (int i = MIN; i <= length; i++)
-    {
-        // if any digit of number is greater than 0, increament check and exit loop
-        if (digits[i] > MIN)
-        {
-            check += 1;
-            break;
-        }
-    }
+	int check = MIN;
 
-    // if check = 0, number is zero    
-    if (check == MIN)
-    {
-        // assign "zero"    
-        strcpy(dWord[MIN], "zero");
+	for (int i = MIN; i <= length; i++)
+	{
+		// if any digit of number is greater than 0, increament check and exit loop
+		if (digits[i] > MIN)
+		{
+			check += 1;
+			break;
+		}
+	}
 
-        return false;
-    }
+	// if check = 0, number is zero    
+	if (check == MIN)
+	{
+		// assign "zero"    
+		strcpy(dWord[MIN], "zero");
 
-    else
-    {
-        for (int i = MIN; i < length; i++)
-        {
-            // converts numbers from 1 - 19 to words
-            if (digits[i] > MIN && digits[i] <= 19)
-                oneNineteen(digits[i], &i);
-																            
-            // converts numbers from 20 - 99 to words
-            else
-            {
-                // isolate unit digit
-                int unit = digits[i] % 10;
-                
-                // isolate tens digit
-                int tens = digits[i] / 10;
+		return false;
+	}
 
-											// convert tens digit to words
-                tensConverter(&tens, &i);
+	else
+	{
+		for (int i = MIN; i < length; i++)
+		{
+			// converts numbers from 1 - 19 to words
+			if (digits[i] > MIN && digits[i] <= 19)
+				oneNineteen(digits[i], &i);
 
-											// convert numbers unit digit to words												
-											oneNineteen(unit, &i);
-            }
-        }
-    }
+			// converts numbers from 20 - 99 to words
+			else
+			{
+				// isolate unit digit
+				int unit = digits[i] % 10;
 
-    return true;
+				// isolate tens digit
+				int tens = digits[i] / 10;
+
+				// convert tens digit to words
+				tensConverter(&tens, &i);
+
+				// convert numbers unit digit to words
+				oneNineteen(unit, &i);
+			}
+		}
+	}
+
+	return true;
 }
 
 /**
@@ -285,53 +285,56 @@ void tensConverter (int *tens, int *i)
  */
 void placeValues(void)
 {
-    for (int i = MIN; i < length; i++)
-    {
-        // identify hundreds with tens and unit e.g 123
-        if (i % 2 != MIN && digits[i] != MIN && digits[i - 1] != MIN)
-            strcat(dWord[i], " hundred and ");
+	for (int i = MIN; i < length; i++)
+	{
+		// identify hundreds with tens and unit e.g 123
+		if (i % 2 != MIN && digits[i] != MIN && digits[i - 1] != MIN)
+			strcat(dWord[i], " hundred and ");
 
-        // identify hundreds with no tens or unit e.g 100
-        if (i % 2 != MIN && digits[i] != MIN && digits[i - 1] == MIN)
-            strcat(dWord[i], " hundred");
-    }
+		// identify hundreds with no tens or unit e.g 100
+		if (i % 2 != MIN && digits[i] != MIN && digits[i - 1] == MIN)
+			strcat(dWord[i], " hundred");
+	}
 
-    // hundreds place has no value e.g 10002 add "and"
-    if ((digits[MIN] > MIN && digits[1] == MIN) && (digits[2] > MIN || digits[3] > MIN || digits[4] > MIN || digits[5] > MIN || digits[6] > MIN || digits[7] > 0))
-        strcat(dWord[1], "and ");
-    
-    // identify and assign thousands,
-    if (digits[2] > MIN || digits[3] > MIN)
-			{
-        // if followed by a hundred add a comma
-        if (digits[1] > MIN)
-            strcat(dWord[2], " thousand, ");
-			    else
-			        strcat(dWord[2], " thousand ");
-			}
-    
-    // identify and assign millions
-    if (digits[4] > MIN || digits[5] > MIN)
-			{
-						// if followed by a digit in hundreds place, add a comma
-        if (digits[3] > MIN || digits[2] > MIN || digits[1] > MIN)
-            strcat(dWord[4], " million, ");
-					 else
-		         strcat(dWord[4], " million ");
-    }
+	// hundreds place has no value e.g 10002 add "and"
+	if ((digits[MIN] > MIN && digits[1] == MIN) && (digits[2] > MIN || digits[3] > MIN || digits[4] > MIN || digits[5] > MIN || digits[6] > MIN || digits[7] > 0))
+		strcat(dWord[1], "and ");
 
-    // identify and assign billions
-    if (digits[6] > MIN || digits[7] > MIN)
-			{
-						// if followed by a digit in hundreds place, add a comma
-        if (digits[5] > MIN || digits[4] > MIN || digits[3] > MIN || digits[2] > MIN || digits[1] > MIN)
-            strcat(dWord[6], " billion, ");
-					 else
-            strcat(dWord[6], " billion ");
-			}
+	// identify and assign thousands,
+	if (digits[2] > MIN || digits[3] > MIN)
+	{
+		// if followed by a hundred add a comma
+		if (digits[1] > MIN)
+			strcat(dWord[2], " thousand, ");
+		
+		else
+			strcat(dWord[2], " thousand ");
+	}
 
-			// add a full stop
-			strcat(dWord[MIN], ".");
+	// identify and assign millions
+	if (digits[4] > MIN || digits[5] > MIN)
+	{
+		// if followed by a digit in hundreds place, add a comma
+		if (digits[3] > MIN || digits[2] > MIN || digits[1] > MIN)
+			strcat(dWord[4], " million, ");
+		
+		else
+			strcat(dWord[4], " million ");
+	}
+
+	// identify and assign billions
+	if (digits[6] > MIN || digits[7] > MIN)
+	{
+		// if followed by a digit in hundreds place, add a comma
+		if (digits[5] > MIN || digits[4] > MIN || digits[3] > MIN || digits[2] > MIN || digits[1] > MIN)
+			strcat(dWord[6], " billion, ");
+		
+		else
+			strcat(dWord[6], " billion ");
+	}
+
+	// add a full stop
+	strcat(dWord[MIN], ".");
 }
 
 /**
@@ -339,14 +342,14 @@ void placeValues(void)
  */
 void print(void)
 {
-			//change first character of first word to upper case
-			dWord[length - 1][MIN] -= DIFF;
-				
-    for (int i = length; i >= 0; i--)
-    {					
-        // print out content of each sub array
-        cout << dWord[i];
-    }
+	//change first character of first word to upper case
+	dWord[length - 1][MIN] -= DIFF;
+
+	for (int i = length; i >= 0; i--)
+	{					
+		// print out content of each sub array
+		cout << dWord[i];
+	}
 }
 
 /**
@@ -355,35 +358,35 @@ void print(void)
  */
 bool runAgain(void)
 {
-			char ans = 'a';
-			bool validate;
-			
-		 // prompt repeatedly until valid response is entered
-		 do
-    {
-         cout << "\n\nRun again?\n"
-              << "Yes (y) or No (n)" << endl;
-						
-						cin >> ans;
-							
-						// constrain reply to 'Y', 'y', 'N' or 'n'	
-				   if (ans != 'Y' && ans != 'y' && ans != 'N' && ans != 'n')
-						{
-						    cout << "\n\nType \"y\" for yes or \"n\" for no" << endl;
-									validate = false;
-									cin.clear();
-									cin.ignore(15, '\n');
-				   }
-								
-			    else
-			        validate = true;
-    	} while (validate == false);
-				
-    if (ans == 'Y' || ans == 'y')
-			    return true;
-			
-			else
-        cout << "\nAll done, Goodbye." << endl;
+	char ans = 'a';
+	bool validate;
 
-    return false;
+	// prompt repeatedly until valid response is entered
+	do
+	{
+		cout << "\n\nRun again?\n"
+		     << "Yes (y) or No (n)" << endl;
+
+		cin >> ans;
+
+		// constrain reply to 'Y', 'y', 'N' or 'n'
+		if (ans != 'Y' && ans != 'y' && ans != 'N' && ans != 'n')
+		{
+			cout << "\n\nType \"y\" for yes or \"n\" for no" << endl;
+			validate = false;
+			cin.clear();
+			cin.ignore(15, '\n');
+		}
+
+		else
+			validate = true;
+	} while (validate == false);
+
+	if (ans == 'Y' || ans == 'y')
+		return true;
+
+	else
+		cout << "\nAll done, Goodbye." << endl;
+
+	return false;
 }
